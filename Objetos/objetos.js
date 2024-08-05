bc8b1d025d2baa3758695ecc0b65ce83a352417d
Claudio Paredes <claudioparedesarbelo@gmail.com>
2024-08-04 22:02:52 -0300

    clase 5 de agosto

--- Objetos/objetos.js
+++ Objetos/objetos.js
@@ -1,4 +1,4 @@
-/* const persona = {
+const persona = {
     nombre: "Claudio", 
     apellido: "Paredes", 
     edad: 44
@@ -29,15 +29,16 @@ const socio2 = new Socio("Jorge", "Garcia", "Muñoz 888", 28964534, 3413589696);
 
 console.log(socio1.nombre)
 console.log(socio2.nombre)
- */
 
-let metodo = "Hola Mundo"
+
+let metodo = "Hola Mundo hoy Domingo"
 
 console.log(metodo.length)//contar los caracteres
 
 console.log(metodo.toUpperCase())//pasa a mayuscula
 
 console.log(metodo.toLowerCase())//pasa a minuscula
+
 
 const productos = {
     marca: "adidas",
@@ -64,3 +65,43 @@ console.log("celular" in persona1)
 for (const propiedad in persona1) {
     console.log(persona1[propiedad]);
 }
+
+for (const key in persona2) {
+    alert(persona2[key])
+    }
+
+
+class Auto{
+    constructor(marca, modelo, combustible){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.combustible = combustible;
+    }
+    hablar(){
+        console.log("Soy un auto marca " + this.marca + " y soy modelo " + this.modelo + " y utilizo combustible " + this.combustible);
+    }
+}
+
+const auto1 = new Auto("fiat", "punto", "diesel");
+
+auto1.hablar()
+
+class Producto{
+    constructor(nombre, precio){
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.vendido = false;
+    }
+    sumarIva() { this.precio = this.precio * 1.21 };
+    vender() {this.vendido = true} ;
+}
+
+const producto1 = new Producto("arroz", "125");
+const producto2 = new Producto("fideo", "50");
+
+producto1.sumarIva();
+console.log(producto1)
+producto2.sumarIva();
+console.log(producto2)
+producto1.vender();
+console.log(producto1)
